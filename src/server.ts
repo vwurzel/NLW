81b9eb1cae29237f157bc3e227e5e8808678ab7c
Vinicius Wurzel <vinicius@wurzel.com.br>
2021-06-23 03:12:45 -0300

    Segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import 'reflect-metadata'
 import express from 'express'
+
+import './database'
+import { router } from './routes'
 
 const app = express()
 
-app.get('/test', (req, res) => {
-    res.send('Testado')
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
